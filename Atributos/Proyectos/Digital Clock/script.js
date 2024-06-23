b98e890ecd642218a12fd05c6195b9e07e64c38f
nicolasvelasquezisaza <nicolasvelasquezisaza@gmail.com>
2024-06-23 04:30:36 +0000

    Corrections on files and Javascript funtions

--- Atributos/Proyectos/Digital Clock/script.js
+++ Atributos/Proyectos/Digital Clock/script.js
@@ -1,14 +1,12 @@
 var hour = document.getElementById("Hora");
 var minute = document.getElementById("Minutos");
 var seconds = document.getElementById("Segundos");
-
 var clock = setInterval(
     function time(){
       var date_now = new Date();
       var hr = date_now.getHours();
       var min = date_now.getMinutes();
-      var sec = date_now.getSeconds();
-      
+      var sec = date_now.getSeconds(); 
     if(hr < 10){
         hr = "0" + hr;
     } 
@@ -18,12 +16,11 @@ var clock = setInterval(
     if(sec < 10){
         sec = "0" + sec;
     }
-
       Hora.textContent = hr;
       Minutos.textContent = min;
       Segundos.textContent = sec;
-
-
-
     },1000
 );
+
+var title = document.getElementById("title");
+title.textContent = hr + ":" + min + ":" + sec;
